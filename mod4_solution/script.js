a20da462ce2820f91095814653ce37c8ea102f41
shrinidhi <72692280+shrinidhi68@users.noreply.github.com>
2020-10-26 21:48:59 +0530

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,16 +1,16 @@
-(function () {
+(function (window) {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
+for (var i in names) {
+  var firstLetter =((names[i]).charAt(0)).toLowerCase();
 
   if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
+    window.byeSpeaker.speak(names[i]);
   }
   else {
-    helloSpeaker(names[i]);
+    window.helloSpeaker.speak(names[i]);
   }
 }
 
-})();
+})(window);
