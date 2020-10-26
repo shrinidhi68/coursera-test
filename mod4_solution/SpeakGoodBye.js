dc07b991c700d235a01b41395c3387563d20196e
shrinidhi <72692280+shrinidhi68@users.noreply.github.com>
2020-10-26 22:11:47 +0530

    Update SpeakGoodBye.js

--- mod4_solution/SpeakGoodBye.js
+++ mod4_solution/SpeakGoodBye.js
@@ -7,7 +7,7 @@
 
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
-// the "speak" method and which you will expose to the global context
+// the "speak" method  which you will expose to the global context
 // See Lecture 52, part 1
 var byeSpeaker = {};
 
